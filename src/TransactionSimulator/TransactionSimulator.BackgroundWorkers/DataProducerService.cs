c1b04483fe0e1b7a369186d7bfa8667474cbc844
Łukasz Zalewski <xlukzalx@gmail.com>
2024-06-06 20:25:02 +0200

    Added publishing transaction data to Kafka

--- src/TransactionSimulator/TransactionSimulator.BackgroundWorkers/DataProducerService.cs
+++ src/TransactionSimulator/TransactionSimulator.BackgroundWorkers/DataProducerService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TransactionSimulator.Services.Interfaces;
 
@@ -8,12 +9,20 @@ public class DataProducerService : BackgroundService
 {
     private readonly ILogger<DataProducerService> _logger;
     private readonly IDataGeneratorService _dataGeneratorService;
+    private readonly IProducer<Null, string> _kafkaProducer;
 
     public DataProducerService(ILogger<DataProducerService> logger,
         IDataGeneratorService dataGeneratorService)
     {
         _logger = logger;
         _dataGeneratorService = dataGeneratorService;
+
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092"
+        };
+
+        _kafkaProducer = new ProducerBuilder<Null, string>(config).Build();
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -42,6 +51,25 @@ public class DataProducerService : BackgroundService
         foreach(var transaction in transactions)
         {
             _logger.LogInformation($"Generated new transaction: userId: {transaction.UserId}, cardId: {transaction.CardId}, value: {transaction.Value}");
+
+            var message = new Message<Null, string>
+            {
+                Value = $"{{\"UserId\": {transaction.UserId}, \"CardId\": {transaction.CardId}, \"Value\": {transaction.Value}, \"Longitude\": {transaction.Longitude}, \"Latitude\": {transaction.Latitude}, \"AvailableLimit\": {transaction.AvailableLimit}}}"
+            };
+
+            _kafkaProducer.Produce("Transakcje", message, deliveryReport =>
+            {
+                if (deliveryReport.Error.IsError)
+                {
+                    _logger.LogError($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                }
+                else
+                {
+                    _logger.LogInformation($"Message delivered to {deliveryReport.TopicPartitionOffset}");
+                }
+            });
         }
+
+        _kafkaProducer.Flush(TimeSpan.FromSeconds(1));
     }
 }
