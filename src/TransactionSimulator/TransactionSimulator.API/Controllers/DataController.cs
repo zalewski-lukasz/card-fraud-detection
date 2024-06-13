9dbfac36cece993c6f7feaa5d339a55ac2f4963a
Łukasz Zalewski <xlukzalx@gmail.com>
2024-06-13 04:12:22 +0200

    Added new endpoint to fetch complete transaction data

--- src/TransactionSimulator/TransactionSimulator.API/Controllers/DataController.cs
+++ src/TransactionSimulator/TransactionSimulator.API/Controllers/DataController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using TransactionSimulator.BackgroundWorkers;
 using TransactionSimulator.Models;
+using TransactionSimulator.Repositories.Interfaces;
 using TransactionSimulator.Services.Interfaces;
 
 namespace TransactionSimulator.API.Controllers
@@ -10,26 +11,36 @@ namespace TransactionSimulator.API.Controllers
     {
         private readonly IDataManagementService _dataManagementService;
         private readonly DataProducerService _producerService;
+        private readonly ITransactionRepository _transactionRepository;
         private readonly ILogger<DataController> _logger;
 
         public DataController(ILogger<DataController> logger,
             IDataManagementService dataManagementService,
-            DataProducerService dataProducerService)
+            DataProducerService dataProducerService,
+            ITransactionRepository transactionRepository)
         {
             _logger = logger;
             _dataManagementService = dataManagementService;
             _producerService = dataProducerService;
+            _transactionRepository = transactionRepository;
         }
 
         [HttpGet]
-        [Route("/api/get-random-users")]
+        [Route("/api/users/random")]
         public IEnumerable<User> GetUsers(int count)
         {
             return _dataManagementService.GetRandomUsers(count);
         }
 
+        [HttpGet]
+        [Route("/api/transactions")]
+        public IEnumerable<Transaction> GetTransactions(int userId, int cardId)
+        {
+            return _transactionRepository.GetTransactionsBuUserAndCard(cardId, userId);
+        }
+
         [HttpPost]
-        [Route("/api/generate-over-the-limit-anomaly")]
+        [Route("/api/anomalies/over-the-limit")]
         public IActionResult GenerateOverTheLimitAnomaly()
         {
             _producerService.GenerateAnomalyTransactions("OVER_THE_LIMIT_ANOMALY");
@@ -37,7 +48,7 @@ namespace TransactionSimulator.API.Controllers
         }
 
         [HttpPost]
-        [Route("/api/generate-multiple-transactions-anomaly")]
+        [Route("/api/anomalies/multiple-transactions")]
         public IActionResult GenerateMultipleTransactionAnomaly()
         {
             _producerService.GenerateAnomalyTransactions("MULTIPLE_TRANSACTIONS_ANOMALY");
@@ -45,7 +56,7 @@ namespace TransactionSimulator.API.Controllers
         }
 
         [HttpPost]
-        [Route("/api/generate-location-anomaly")]
+        [Route("/api/anomalies/location-change")]
         public IActionResult GenerateLocationAnomaly()
         {
             _producerService.GenerateAnomalyTransactions("LOCATION_ANOMALY");
